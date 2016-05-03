81428ee1987fa818ef5db24eaa00b290e2ff9fd5
Suchan An <me@ansuchan.com>
2016-05-03 12:14:59 +0900

    update router.js: read partial templates and merge templates

--- 20160427_API를_이용한_CLI_와_웹_어플리케이션/router.js
+++ 20160427_API를_이용한_CLI_와_웹_어플리케이션/router.js
@@ -1,6 +1,18 @@
+var fs = require('fs');
+
+
 function home(request, response) {
   if (request.url === "/") {
-    response.write("home");
+    var baseContent = fs.readFileSync("./templates/base.html", "utf8");
+    var headerContent = fs.readFileSync("./templates/header.html", "utf8");
+    var footerContent = fs.readFileSync("./templates/footer.html", "utf8");
+    var mainContent = fs.readFileSync("./templates/home.html", "utf8");
+
+    mergedContent = baseContent.replace("{{ header }}", headerContent);
+    mergedContent = mergedContent.replace("{{ footer }}", footerContent);
+    mergedContent = mergedContent.replace("{{ content }}", mainContent);
+
+    response.write(mergedContent);
     response.end();
   }
 }
