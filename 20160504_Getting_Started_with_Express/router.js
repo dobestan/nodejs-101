cf704da8e694e1fd2ef8bb06c1b08dbce40acca6
Suchan An <me@ansuchan.com>
2016-05-03 20:55:44 +0900

    update router.js: add room detail router

--- 20160504_Getting_Started_with_Express/router.js
+++ 20160504_Getting_Started_with_Express/router.js
@@ -8,5 +8,10 @@ router.get('/', function(request, response) {
   response.send("hello world");
 });
 
+router.get('/:roomId', function(request, response) {
+  var roomId = request.params.roomId;
+  response.send(roomId);
+});
+
 
 module.exports = router;
