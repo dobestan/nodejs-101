7d31a46c2c1db4357b4766024b73cc4b318c6454
Suchan An <me@ansuchan.com>
2016-05-03 12:18:28 +0900

    refactor router.js: refactor render function on renderer.js

---
+++ 20160427_API를_이용한_CLI_와_웹_어플리케이션/renderer.js
@@ -0,0 +1,19 @@
+var fs = require('fs');
+
+
+function render(templateName, values, response) {
+  var baseContent = fs.readFileSync("./templates/base.html", "utf8");
+  var headerContent = fs.readFileSync("./templates/header.html", "utf8");
+  var footerContent = fs.readFileSync("./templates/footer.html", "utf8");
+  var mainContent = fs.readFileSync("./templates/" + templateName + ".html", "utf8");
+
+  mergedContent = baseContent.replace("{{ header }}", headerContent);
+  mergedContent = mergedContent.replace("{{ footer }}", footerContent);
+  mergedContent = mergedContent.replace("{{ content }}", mainContent);
+
+  response.write(mergedContent);
+  response.end();
+}
+
+
+module.exports = render;
--- 20160427_API를_이용한_CLI_와_웹_어플리케이션/router.js
+++ 20160427_API를_이용한_CLI_와_웹_어플리케이션/router.js
@@ -1,19 +1,9 @@
-var fs = require('fs');
+var render = require('./renderer');
 
 
 function home(request, response) {
   if (request.url === "/") {
-    var baseContent = fs.readFileSync("./templates/base.html", "utf8");
-    var headerContent = fs.readFileSync("./templates/header.html", "utf8");
-    var footerContent = fs.readFileSync("./templates/footer.html", "utf8");
-    var mainContent = fs.readFileSync("./templates/home.html", "utf8");
-
-    mergedContent = baseContent.replace("{{ header }}", headerContent);
-    mergedContent = mergedContent.replace("{{ footer }}", footerContent);
-    mergedContent = mergedContent.replace("{{ content }}", mainContent);
-
-    response.write(mergedContent);
-    response.end();
+    render("home", {}, response);
   }
 }
 
@@ -21,8 +11,7 @@ function home(request, response) {
 function room(request, response) {
   var roomId = request.url.replace("/", "");
   if (roomId.length > 0) {
-    response.write(roomId);
-    response.end();
+    render("room", {}, response);
   }
 }
 
