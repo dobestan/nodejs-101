1486ee2ba0a50468a43a120dd484ef67e488fafe
Suchan An <me@ansuchan.com>
2016-05-25 10:28:53 +0900

    update users html page: list, detail

--- 20160525_node_with_mongodb/routes/users.js
+++ 20160525_node_with_mongodb/routes/users.js
@@ -4,7 +4,18 @@ var router = express.Router();
 
 
 router.get("/", function(request, response) {
-  response.render("users/list");
+  request.db.get("users").find({}, function(error, document) {
+    return response.render("users/list", users=document);
+  });
+});
+
+
+router.get("/:username", function(request, response) {
+  var username = request.params.username;
+
+  request.db.get("users").find({username: username}, function(error, document) {
+    return response.render("users/detail", user=document[0]);
+  });
 });
 
 
