7d39cba199eb747e69f107b778454539921bc3d2
Suchan An <me@ansuchan.com>
2016-05-25 10:46:16 +0900

    add posts create functionality

--- 20160525_node_with_mongodb/routes/posts.js
+++ 20160525_node_with_mongodb/routes/posts.js
@@ -12,4 +12,31 @@ router.get("/", function(request, response) {
 });
 
 
+router.post("/", function(request, response) {
+  var title = request.body.title;
+  var content = request.body.content;
+
+  request.db.get("posts").insert({title: title, content: content}, function(error, document) {
+    if (error) console.log(error);
+    return response.redirect("/posts/");
+  });
+});
+
+
+router.get("/new", function(request, response) {
+  return response.render(
+    "posts/new"
+  );
+});
+
+
+router.get("/:title", function(request, response) {
+  var title = request.params.title;
+
+  request.db.get("posts").find({title: title}, function(error, document) {
+    return response.render("posts/detail", {post: document[0]});
+  });
+});
+
+
 module.exports = router;
