276aed155ebb76804db13dce8f148a97bab3c9ce
Suchan An <me@ansuchan.com>
2016-05-25 09:22:14 +0900

    update users router: implement retrieve functionality

--- 20160525_node_with_mongodb/routes/users.js
+++ 20160525_node_with_mongodb/routes/users.js
@@ -11,4 +11,14 @@ router.get('/', function(request, response) {
 });
 
 
+router.get('/:userId', function(request, response) {
+  var userId = Number(request.params.userId);
+
+  request.db.get("users").find({'userId': userId}, function(error, document) {
+    if (error) console.log(error);
+    response.send(document);
+  });
+});
+
+
 module.exports = router;
