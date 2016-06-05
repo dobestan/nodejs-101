9c1ef1afc3d0b47d4cb35407885db88061b971dd
Suchan An <me@ansuchan.com>
2016-06-05 22:57:01 +0900

    init passport settings

--- 20160608_passport/app.js
+++ 20160608_passport/app.js
@@ -1,8 +1,29 @@
 var express = require('express');
 var mongoose = require('mongoose');
+var expressSession = require('express-session');
 
 
 var app = express();
+
+
+var User = require("./models/user");
+
+
+app.use(expressSession({secret: "Nodecamp is Awesome"}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+// Configure Passport
+passport.serializeUser(function(user, done) {
+  done(null, user._id);
+});
+
+passport.deserializeUser(function(id, done) {
+  User.findById(id, function(error, user) {
+    done(error, user);
+  });
+});
 
 
 mongoose.connect("mongodb://localhost/passport");
