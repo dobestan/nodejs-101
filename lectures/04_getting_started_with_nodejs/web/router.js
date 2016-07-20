8337a16d110b868502021bae18c8256a9f7e4a9f
Suchan An <me@ansuchan.com>
2016-07-20 18:31:51 +0900

    update lecture #4: implement simple zigbang api crawling

--- lectures/04_getting_started_with_nodejs/web/router.js
+++ lectures/04_getting_started_with_nodejs/web/router.js
@@ -1,3 +1,6 @@
+var http = require("http");
+
+
 function homeRouter(request, response) {
   if (request.url === "/") {
     response.write("<h1>Home</h1>");
@@ -9,8 +12,19 @@ function homeRouter(request, response) {
 function roomRouter(request, response) {
   var roomId = request.url.replace("/", "");
   if (roomId.length > 0) {
-    response.write(roomId);
-    response.end();
+    var zigbangApiUrl = "http://api.zigbang.com/v1/items?detail=true&item_ids=" + roomId;
+    http.get(zigbangApiUrl, function(apiResponse) {
+      var data = "";
+
+      apiResponse.on("data", function(chunk) {
+        data += chunk;
+      });
+
+      apiResponse.on("end", function() {
+        response.write(data);
+        response.end();
+      });
+    });
   }
 }
 
