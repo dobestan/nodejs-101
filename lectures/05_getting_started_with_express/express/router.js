46e03bbd7d46d893314ef79d18f8ae8fc467ed55
Suchan An <me@ansuchan.com>
2016-07-25 00:54:54 +0900

    update lecture #5: add watcha router

--- lectures/05_getting_started_with_express/express/router.js
+++ lectures/05_getting_started_with_express/express/router.js
@@ -1,3 +1,4 @@
+var httpRequest = require("request");
 var express = require("express");
 
 var router = express.Router();
@@ -8,6 +9,20 @@ router.get("/", function(request, response) {
     "home",
     {
       title: "Node.js 로 시작하는 웹 프로그래밍"
+    }
+  );
+});
+
+
+router.get("/watcha/", function(request, response) {
+  httpRequest.get(
+    "https://watcha.net/home/news.json?page=1&per=20",
+    function(error, httpResponse, body) {
+      var data = JSON.parse(body);
+
+      return response.render("watcha", {
+        news: data.news
+      });
     }
   );
 });
