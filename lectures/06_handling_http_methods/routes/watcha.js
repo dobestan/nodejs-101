7dda3471657fc10e4459096dcbf9ed57f1a5b05f
Suchan An <me@ansuchan.com>
2016-07-27 11:43:47 +0900

    update lecture #6: implement watcha search feature

--- lectures/06_handling_http_methods/routes/watcha.js
+++ lectures/06_handling_http_methods/routes/watcha.js
@@ -5,10 +5,26 @@ var httpRequest = require("request");
 
 
 router.get("/", function(request, response) {
+  var query = request.query.query;
+
   var watchaApiUrl = "https://watcha.net/home/news.json?page=1&per=20";
   httpRequest(watchaApiUrl, function(error, httpResponse, body) {
     var data = JSON.parse(body);
-    return response.render("watcha", {newsItems: data.news});
+    var newsItems = data.news;
+
+    if (query) {
+      var matchedNewsItems = [];
+
+      newsItems.forEach(function(newsItem) {
+        if (newsItem.title.indexOf(query) > -1) {
+          matchedNewsItems.push(newsItem);
+        }
+      });
+
+      newsItems = matchedNewsItems;
+    }
+
+    return response.render("watcha", {newsItems: newsItems, query: query});
   });
 });
 
