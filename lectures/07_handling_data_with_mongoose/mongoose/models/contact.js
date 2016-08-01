4a380aafc9275d34193cf6bb6247183af5d189ee
Suchan An <me@ansuchan.com>
2016-08-01 19:00:13 +0900

    update lecture #7: add simple pre hooks - manage createdAt, updatedAt field

--- lectures/07_handling_data_with_mongoose/mongoose/models/contact.js
+++ lectures/07_handling_data_with_mongoose/mongoose/models/contact.js
@@ -6,7 +6,19 @@ var contactSchema = new Schema({
   name: String,
   email: String,
   phonenumber: String,
-  createdAt: Date
+  createdAt: Date,
+  updatedAt: Date
+});
+
+
+contactSchema.pre("save", function(next) {
+  var currentDate = new Date();
+  if (!this.createdAt) this.createdAt = currentDate;
+  this.updatedAt = currentDate;
+
+  console.log("새로운 연락처가 생성되었습니다.");
+
+  next();
 });
 
 
