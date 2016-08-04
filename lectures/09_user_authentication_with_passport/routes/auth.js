b91bfaa3e34e12b05fad694f85f0043352c0b4b3
Suchan An <me@ansuchan.com>
2016-08-04 15:20:23 +0900

    update lecture #9: refactor router using route, all method

--- lectures/09_user_authentication_with_passport/routes/auth.js
+++ lectures/09_user_authentication_with_passport/routes/auth.js
@@ -6,47 +6,46 @@ var User = require("../models/user");
 var authMiddleware = require("../middlewares/auth");
 
 
-router.get("/login/", authMiddleware.logoutRequired, function(request, response) {
+router.route("/login/")
+  .all(authMiddleware.logoutRequired)
+  .get(function(request, response) {
     return response.render("auth/login");
-});
+  })
+  .post(function(request, response, next) {
+    var username = request.body.username;
+    var password = request.body.password;
 
+    User.authenticate(username, password, function(error, user) {
+      if (error) return next(error);
 
-router.post("/login/", function(request, response, next) {
-  var username = request.body.username;
-  var password = request.body.password;
+      request.session.user = user;
+      request.flash("success", "성공적으로 로그인 되었습니다.");
 
-  User.authenticate(username, password, function(error, user) {
-    if (error) return next(error);
-
-    request.session.user = user;
-    request.flash("success", "성공적으로 로그인 되었습니다.");
-
-    return response.redirect("/");
-  });
-});
-
-
-router.get("/signup/", authMiddleware.logoutRequired, function(request, response) {
-  return response.render("auth/signup");
-});
-
-
-router.post("/signup/", function(request, response, next) {
-  // FIXME: should validate user
-
-  var user = new User({
-    username: request.body.username,
-    password: request.body.password,
-    email: request.body.email,
-    phonenumber: request.body.phonenumber
+      return response.redirect("/");
+    });
   });
 
-  user.save(function(error, user) {
-    if (error) return next(error);
-    return response.redirect("/");
-  });
-});
 
+router.route("/signup/")
+  .all(authMiddleware.logoutRequired)
+  .get(function(request, response) {
+    return response.render("auth/signup");
+  })
+  .post(function(request, response, next) {
+    // FIXME: should validate user
+
+    var user = new User({
+      username: request.body.username,
+      password: request.body.password,
+      email: request.body.email,
+      phonenumber: request.body.phonenumber
+    });
+
+    user.save(function(error, user) {
+      if (error) return next(error);
+      return response.redirect("/");
+    });
+  });
 
 
 router.get("/logout/", function(request, response) {
