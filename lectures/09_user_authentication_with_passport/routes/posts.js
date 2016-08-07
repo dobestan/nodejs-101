711eb469c694f8c7b1091b2022ffaf75dd57254c
Suchan An <me@ansuchan.com>
2016-08-07 15:12:47 +0900

    update lecture #9: add simple post create functionality

--- lectures/09_user_authentication_with_passport/routes/posts.js
+++ lectures/09_user_authentication_with_passport/routes/posts.js
@@ -4,11 +4,31 @@ var router = express.Router();
 var Post = require("../models/post");
 
 
-router.get("/", function(request, response, next) {
-  Post.find({}, function(error, posts) {
-    if (error) return next(error);
-    return response.render("posts/list", {posts: posts});
+router.route("/")
+  .get(function(request, response, next) {
+    Post.find({}, function(error, posts) {
+      if (error) return next(error);
+      return response.render("posts/list", {posts: posts});
+    });
+  })
+  .post(function(request, response, next) {
+    var title = request.body.title;
+    var content = request.body.content;
+
+    var post = new Post({
+      title: title,
+      content: content
+    });
+
+    post.save(function(error, post) {
+      if (error) return next(error);
+      return response.redirect("/posts/");
+    });
   });
+
+
+router.get("/new/", function(request, response) {
+  return response.render("posts/new", {});
 });
 
 
