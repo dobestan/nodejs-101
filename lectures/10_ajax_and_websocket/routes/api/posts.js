efb7e86a15fdc86aff7c6faae7e1137d7cfb0fe4
Suchan An <me@ansuchan.com>
2016-08-09 21:27:38 +0900

    update lecture #10: add simple posts:create api

--- lectures/10_ajax_and_websocket/routes/api/posts.js
+++ lectures/10_ajax_and_websocket/routes/api/posts.js
@@ -9,6 +9,19 @@ router.route("/")
     Post.find({}, function(error, posts) {
       return response.json(posts);
     });
+  })
+  .post(function(request, response, next) {
+    var title = request.body.title;
+    var content = request.body.content;
+
+    var post = new Post({
+      title: title,
+      content: content
+    });
+
+    post.save(function(error, post) {
+      return response.status(201).json("Successfully created");
+    });
   });
 
 
