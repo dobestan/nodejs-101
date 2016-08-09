c41146d0bd185af7391db90155803c58be1fe87a
Suchan An <me@ansuchan.com>
2016-08-09 23:03:10 +0900

    update lecture #10: refactor socketio example

--- lectures/10_ajax_and_websocket/app.js
+++ lectures/10_ajax_and_websocket/app.js
@@ -82,18 +82,7 @@ app.use(function(error, request, response, next) {
 });
 
 
-io.on("connection", function(socket) {
-  console.log("Socket is connected");
-
-  socket.on("chat", function(message) {
-    console.log("Chat: " + message);
-    io.emit("chat", message);
-  });
-
-  socket.on("disconnect", function() {
-    console.log("Socket is disconnected");
-  });
-});
+require("./socket")(io);
 
 
 httpServer.listen(3000, function() {
