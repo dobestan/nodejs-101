b7c2755eaa4fc33e6de3e13677c9df7941eb222c
Suchan An <me@ansuchan.com>
2016-08-11 17:27:31 +0900

    update koscom #4: fix loginRequired middleware

--- koscom/02_express/middlewares/auth.js
+++ koscom/02_express/middlewares/auth.js
@@ -1,6 +1,6 @@
 function loginRequired() {
   return function(req, res, next) {
-    if (!req.session.user) {
+    if (!req.user) {
       req.flash("error", "로그인이 필요한 페이지입니다.");
       return res.redirect("/login/?next=" + req.originalUrl);
     }
