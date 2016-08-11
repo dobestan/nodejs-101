f5a6c15674ff3a45c5766bae2b34136408987928
Suchan An <me@ansuchan.com>
2016-08-11 23:23:59 +0900

    update jwt example: add verify router

--- koscom/02_express/routes/api/auth.js
+++ koscom/02_express/routes/api/auth.js
@@ -12,9 +12,21 @@ router.route("/")
 
     User.authenticate()(username, password, function(error, user) {
       if (error) return next(error);
+      console.log(user._doc);
       if (!user) return res.status(400).send({message: "유저 정보가 올바르지 않습니다."});
-      var token = jwt.sign(user, app.get("jwtSecret"));
+      var token = jwt.sign(user._doc, req.app.get("jwtSecret"));
       return res.json({token: token});
+    });
+  });
+
+
+router.route("/verify/")
+  .post(function(req, res, next) {
+    var token = req.body.token; // FIXME: Authorization: Bearer ...
+
+    jwt.verify(token, req.app.get("jwtSecret"), function(error, user) {
+      if (error) return res.status(400).json(error);
+      return response.status(200).json(user);
     });
   });
 
