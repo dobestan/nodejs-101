51d214f4bf5e8e066a59e875db4dcb5b303d31a6
Suchan An <me@ansuchan.com>
2016-10-06 16:38:22 +0900

    update renderer: add base template extends feature

--- koscom/2nd/server/renderer.js
+++ koscom/2nd/server/renderer.js
@@ -2,9 +2,18 @@ var path = require("path");
 var fs = require("fs");
 
 
-module.exports = function(templateName) {
+module.exports = function(templateName, baseTemplateName) {
+  var baseTemplateName = baseTemplateName || "base";
+  var baseTemplateFilePath = path.join(__dirname, "templates", baseTemplateName + ".html");
+  var baseContent = fs.readFileSync(baseTemplateFilePath, "utf8");
+
   var templateFilePath = path.join(__dirname, "templates", templateName + ".html");
   var content = fs.readFileSync(templateFilePath, "utf8");
+
+  content = baseContent.replace(
+    "{{ content }}",
+    content   // template 별로 존재하는 HTML 파일
+  );
 
   return content;
 }
