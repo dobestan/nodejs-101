9b00fdbb096bdf486d5c2c78b5c5a0bd9269cdae
Suchan An <me@ansuchan.com>
2016-10-06 16:14:33 +0900

    update renderer: add fs.readFileSync

--- koscom/2nd/server/controllers/home.js
+++ koscom/2nd/server/controllers/home.js
@@ -4,7 +4,8 @@ var fs = require("fs");
 
 module.exports = function(req, res) {
   var templateFilePath = path.join(__dirname, "..", "templates", "home.html");
+  var content = fs.readFileSync(templateFilePath, "utf8");
 
-  res.write(templateFilePath);
+  res.write(content);
   res.end();
 }
