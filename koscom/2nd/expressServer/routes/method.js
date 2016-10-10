decaa14cf51e7ec88687aed8b15fc20f19bd5cb2
Suchan An <me@ansuchan.com>
2016-10-10 16:41:28 +0900

    update method.js: refactor routers via router.route

--- koscom/2nd/expressServer/routes/method.js
+++ koscom/2nd/expressServer/routes/method.js
@@ -1,18 +1,24 @@
-
-
-
 var express = require("express");
 var router = express.Router();
 
 
-router.get("/", function(req, res, next) {
-  return res.json(req.query);
-});
-
-
-router.post("/", function(req, res, next) {
-  return res.json(req.body);
-});
+router.route("/")
+  .all(function(req, res, next) {
+    console.log("hello HTTP Method");
+    next();
+  })
+  .get(function(req, res, next) {
+    return res.json(req.query);
+  })
+  .post(function(req, res, next) {
+    return res.json(req.body);
+  })
+  .patch(function(req, res, next) {
+    return res.send("PATCH");
+  })
+  .delete(function(req, res, next) {
+    return res.send("DELETE");
+  });
 
 
 
