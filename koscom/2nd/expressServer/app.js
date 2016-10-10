85a5ddc5b6d03ec087beebea034fac1761c9d1a5
Suchan An <me@ansuchan.com>
2016-10-10 09:49:19 +0900

    init package.json with express

---
+++ koscom/2nd/expressServer/app.js
@@ -0,0 +1,10 @@
+// https://github.com/dobestan/nodejs-101/tree/master/koscom/2nd/express
+//
+//
+// $ npm init    // package.json 초기화
+// $ npm install --save express    // express 설치 + 동시에 pakcage.json 에 기록
+
+
+var express = require("express");
+
+
