ca21a48e321def2122ce0d9ad7ddd9659950f5a6
Suchan An <me@ansuchan.com>
2016-10-10 14:13:04 +0900

    update watcha, watcha_test

--- koscom/2nd/expressServer/utils/watcha.js
+++ koscom/2nd/expressServer/utils/watcha.js
@@ -2,7 +2,7 @@ var request = require("request");
 
 
 module.exports = function (page, per, callback) {
-  var url = "https://watcha.net/home/news.json?page=1&per=5";
+  var url = "https://watcha.net/home/news.json?page=" + page + "&per=" + per;
 
   request(url, function(error, response, body) {
     if (error) return callback(error, null);
---
+++ koscom/2nd/expressServer/watcha_test.js
@@ -0,0 +1,6 @@
+var watcha = require("./utils/watcha");
+
+
+watcha(1, 100, function(error, data) {
+  console.log(data);
+});
