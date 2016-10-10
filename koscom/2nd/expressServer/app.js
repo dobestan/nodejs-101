03abd4b6869e1c31550caca3c932dc93fdf3f65f
Suchan An <me@ansuchan.com>
2016-10-10 10:13:38 +0900

    init app.js: simple middleware and router

--- koscom/2nd/expressServer/app.js
+++ koscom/2nd/expressServer/app.js
@@ -5,6 +5,31 @@
 // $ npm install --save express    // express 설치 + 동시에 pakcage.json 에 기록
 
 
-var express = require("express");
+var express = require("express");   // http.createServer
+
+var app = express();
+
+// npm install -g nodemon
+// nodemon app.js ( nodemon 켜져있는 곳에서 "rs + Enter" )
 
 
+var logger = function(req, res, next) {
+  console.log("Request on", req.url, "at", new Date());
+  next();
+}
+app.use(logger);
+
+
+app.get("/", function(req, res, next) {
+  return res.send("home");
+});
+
+
+app.get("/about", function(req, res, next) {
+  return res.send("about");
+});
+
+
+app.listen(3000, function() {
+  console.log("Server is running");
+});
