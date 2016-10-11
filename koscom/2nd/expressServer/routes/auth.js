dc62e75ef7d1b9bb8884bf27a4d8b4f4bd187972
Suchan An <me@ansuchan.com>
2016-10-11 13:38:32 +0900

    init mypage

--- koscom/2nd/expressServer/routes/auth.js
+++ koscom/2nd/expressServer/routes/auth.js
@@ -44,4 +44,20 @@ router.route("/logout")
   })
 
 
+router.route("/mypage")
+  .get(
+    function(req, res, next) {
+      if (req.session.user) {
+        next();
+      } else {
+        req.flash("error", "로그인이 필요한 페이지입니다.");
+        return res.redirect("/login");
+      }
+    },
+    function(req, res, next) {
+      return res.send("This is my page");
+    }
+  );
+
+
 module.exports = router;
