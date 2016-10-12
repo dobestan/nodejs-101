35483f996d985e7ac25485101b519804e4911338
Suchan An <me@ansuchan.com>
2016-10-12 15:44:28 +0900

    update posts:create ajax example

--- koscom/2nd/expressServer/public/js/posts.js
+++ koscom/2nd/expressServer/public/js/posts.js
@@ -1,1 +1,37 @@
-alert("hello world");
+(function() {
+  $(document).ready(function() {
+    var postsElement = $("#posts");
+
+    var formElement = $("form");
+    var titleElement = $(formElement).find("input[name='title']");
+    var contentElement = $(formElement).find("input[name='content']");
+
+    var url = "/api/posts/";
+
+    formElement.submit(function() {
+      var title = $(titleElement).val(); // jQuery Function => getter, setter
+      var content = $(contentElement).val();
+      var data = {
+        title: title,
+        content: content
+      };
+
+      $.ajax({
+        url: url,
+        data: data,
+        type: "POST",
+        success: function(result) {
+          alert(result);
+          $(postsElement).append("<li>" + title + "</li>");
+        }
+      });
+
+      $(titleElement).val("");
+      $(contentElement).val("");
+
+      return false;
+    });
+  });
+})();
+
+
