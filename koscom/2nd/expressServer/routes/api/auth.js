879f838e4d20faf19dcbf8f72d8932abdc38dc42
Suchan An <me@ansuchan.com>
2016-10-12 14:39:57 +0900

    add apiLoginRequired Middleware: get token from querystring, http body, or header

--- koscom/2nd/expressServer/routes/api/auth.js
+++ koscom/2nd/expressServer/routes/api/auth.js
@@ -28,6 +28,29 @@ router.post("/verify", function(req, res, next) {
 });
 
 
+function apiLoginRequired() {
+  return function(req, res, next) {
+    var token = req.body.token || req.query.token || req.headers["x-jwt-token"];
+
+    if (!token) {
+      return res.status(401).send("Token is required");
+    }
+
+    jwt.verify(token, "KOSCOM Node.js", function(error, decoded) {
+      if (error) return res.json(error);
+      next();
+    });
+  }
+}
+
+
+router.get("/secret",
+  apiLoginRequired(),
+  function(req, res, next) {
+    return res.send("very secret info");
+  }
+);
+
 
 
 
