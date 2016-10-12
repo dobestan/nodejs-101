18c68444dc489f127f449d6cd1481e4bef77cc39
Suchan An <me@ansuchan.com>
2016-10-12 13:03:13 +0900

    update passport config: add passport-facebook strategy

--- koscom/2nd/expressServer/config/passport.js
+++ koscom/2nd/expressServer/config/passport.js
@@ -12,4 +12,25 @@ module.exports = function(passport) {
   // 1. Local Strategy 를 구현하는 부분
   // 2. User.authenticate 함수를 수정하는 부분
   // 3. auth.js Router 에서 passport.authenticate Middleware 를 사용
+
+  passport.use(new FacebookStrategy({
+    clientID: "1139858509419307",
+    clientSecret: "1dcaab1854d47b009912b1c43288f62d",
+    callbackURL: "http://localhost:3000/auth/facebook/callback"
+  }, function(accessToken, refreshToken, profile, callback) {
+    User.findOne({username: profile.id}, function(error, user) {
+      if (error) return callback(error);
+      if (user) return callback(error, user);
+
+      var user = new User({
+        username: profile.id,
+        password: profile.id,
+        email: profile.id + "@facebook.com"
+      });
+
+      user.save(function(error, user) {
+        return callback(error, user);
+      });
+    });
+  }));
 }
